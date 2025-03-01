f42846f74f20a9ddabede09aadab0323b05abbb2
Marlon <marlonsanchezal@gmail.com>
2025-03-01 16:42:38 -0600

    Se cambio la logica del sistema, ahora recibe nombres y apellidos

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { useState } from 'react';
 
 export default function Home() {
-  const [nombre, setNombre] = useState("");
+  const [nombres, setNombres] = useState("");
+  const [apellidos, setApellidos] = useState("");
   const [mesa, setMesa] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -15,10 +16,13 @@ export default function Home() {
     setLoading(true);
 
     try {
-      const response = await fetch(`/api/getTable?nombre=${encodeURIComponent(nombre)}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
+      const response = await fetch(
+        `/api/getTable?nombres=${encodeURIComponent(nombres)}&apellidos=${encodeURIComponent(apellidos)}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
 
       const data = await response.json();
       if (response.ok) {
@@ -27,7 +31,7 @@ export default function Home() {
         setError(data.error);
       }
     } catch (error) {
-      console.log('Error al enviar los datos: ', error);
+      console.log("Error al enviar los datos: ", error);
       setError("Error al conectar con el servidor");
     } finally {
       setLoading(false);
@@ -56,35 +60,40 @@ export default function Home() {
             priority
           />
         </div>
-        <div className='p-10 flex flex-col justify-center md:mt-28 sm:mt-20 gap-3 text-center'>
+        <div className="p-10 flex flex-col justify-center md:mt-28 sm:mt-20 gap-3 text-center">
           <div className="flex flex-col">
             <h1 className="text-4xl font-bold" style={{ color: "#6c8850" }}>Distribución de mesas</h1>
-            <p className="text-gray-600 text-lg">Ingresá tu nombre para obtener tu número de mesa</p>
+            <p className="text-gray-600 text-lg">Ingresá tu nombre y apellido para obtener tu número de mesa</p>
           </div>
-          {mesa && <p className="my-2 text-amber-600 text-xl" style={{ color: '#af7b50'}}>Tu mesa asignada es: {mesa}</p>}
+          {mesa && <p className="my-2 text-amber-600 text-xl" style={{ color: '#af7b50' }}>Tu mesa asignada es: {mesa}</p>}
           {error && <p className="mt-4 text-red-500">{error}</p>}
-          <form onSubmit={handleSubmit} className='flex flex-col gap-2 w-full'>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-full">
             <input
               type="text"
-              placeholder="Nombre completo"
-              className="mt-4 w-full px-4 py-2 border rounded-lg shadow-md text-brown-700"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
+              placeholder="Nombres"
+              className="w-full px-4 py-2 border rounded-lg shadow-md text-brown-700"
+              value={nombres}
+              onChange={(e) => setNombres(e.target.value)}
             />
-        
-            <div className='flex flex-col justify-center w-full'>
+            <input
+              type="text"
+              placeholder="Apellidos"
+              className="w-full px-4 py-2 border rounded-lg shadow-md text-brown-700"
+              value={apellidos}
+              onChange={(e) => setApellidos(e.target.value)}
+            />
+            <div className="flex flex-col justify-center w-full">
               <button
                 type="submit"
                 className="mt-4 w-full text-white py-2 rounded-lg shadow-md"
                 style={{ backgroundColor: "#af7b50" }}
                 disabled={loading}
               >
-                {loading ? 'Cargando...' : 'Aceptar'}
+                {loading ? "Cargando..." : "Aceptar"}
               </button>
               <a
                 href="https://infobodasd.webflow.io"
                 className="mt-4 w-full text-yellow-950 py-2 rounded-lg text-center shadow-md"
-
                 style={{ backgroundColor: "#fff5eb", color: "#af7b50", display: "block", padding: "10px", textDecoration: "none" }}
               >
                 Cancelar
